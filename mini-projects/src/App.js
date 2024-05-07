e6bc134b632525b40c03ac1d59262cc5ea7b8eda
Gitstar-OC <chandankarom07@gmail.com>
2024-05-07 17:40:46 +0530

    Deleted unused css file as component in styled inside code itself, and removed useless nesting

--- mini-projects/src/App.js
+++ mini-projects/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import React from'react';
 import Accordian from './Accordian/Accordian';
+import RCG from './RCG'
+
 
 function App() {
     return (
+      <>
       <Accordian /> 
+      <RCG />
+      </>
   )
 }
 
--- mini-projects/src/App.test.js
+++ mini-projects/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Testing Material/);
+  const linkElement = screen.getByText(/Accordian/);
   expect(linkElement).toBeInTheDocument();
 });
